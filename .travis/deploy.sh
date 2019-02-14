5f0c450d62b056f2a252ef62a9eba7403f9185dc
alexchomiak <alex.chomiak@gmail.com>
2019-02-13 21:15:16 -0600

    again?

--- .travis/deploy.sh
+++ .travis/deploy.sh
@@ -4,21 +4,13 @@
 set -xe
 
 if [ $TRAVIS_BRANCH == "master" ] ; then
-
+	echo "deploying"
     # setup ssh agent, git config and remote
     eval "$(ssh-agent -s)"
     ssh-add ~/.ssh/travis_rsa
+	git config --global push.default matching
     git remote add deploy "travis@chom.es:/home/travis/docker-react"
-    git config user.name "Travis CI"
-    git config user.email "alex.chomiak@gmail.com"
-
-    # commit compressed files and push it to remote
-    #rm -f .gitignore
-    #cp .travis/deployignore .gitignore
-    git add .
-    git status # debug
-    git commit -m "Deploy compressed files"
-    git push -f deploy HEAD:master
-else
-echo "oof"
+	git push deploy master
+else   	
+	echo "oof"
 fi
