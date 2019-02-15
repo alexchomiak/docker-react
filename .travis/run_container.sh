f94da1cc449fae8d35a4d8d4af02188609547d7c
alexchomiak <alex.chomiak@gmail.com>
2019-02-15 01:53:58 -0600

    test

--- .travis/run_container.sh
+++ .travis/run_container.sh
@@ -4,5 +4,5 @@ npm i
 npm run build
 docker stop $(docker ps -q --filter ancestor=dockerreactprod )
 docker build -t dockerreactprod .
-screen -S docker-react docker run -p 5678:80 dockerreactprod
+screen -dmS dockerreactprod docker run -p 5678:80 dockerreactprod
 echo "started"
