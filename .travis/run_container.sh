31eed173e9843ecaafdb472f20c226b55d9e6077
alexchomiak <alex.chomiak@gmail.com>
2019-02-15 02:22:00 -0600

    testing travis hang

--- .travis/run_container.sh
+++ .travis/run_container.sh
@@ -1,9 +1,8 @@
 #!/bin/bash
 cd /home/travis/docker-react
-#pwd
-#npm i
-#npm run build
-#docker stop $(docker ps -q --filter ancestor=dockerreactprod )
-#docker build -t dockerreactprod .
-#screen -dmS dockerreactprod docker run -p 5678:80 dockerreactprod
+npm i
+npm run build
+docker stop $(docker ps -q --filter ancestor=dockerreactprod )
+docker build -t dockerreactprod .
+screen -dmS dockerreactprod docker run -p 5678:80 dockerreactprod
 exit
