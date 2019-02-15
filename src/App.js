0648f397343d2c8fb4ec3e8ca8781e7df44faedd
alexchomiak <alex.chomiak@gmail.com>
2019-02-15 02:07:30 -0600

    yee

--- src/App.js
+++ src/App.js
@@ -17,7 +17,7 @@ class App extends Component {
             target="_blank"
             rel="noopener noreferrer"
           >
-            oooof........
+            oooof this was pushed by travisCI........
           </a>
         </header>
       </div>
