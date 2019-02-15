ad97b99fa19ff73b474bd69f1293c01f6bf33e32
alexchomiak <alex.chomiak@gmail.com>
2019-02-15 10:40:45 -0600

    AJAX

--- src/App.js
+++ src/App.js
@@ -17,7 +17,7 @@ class App extends Component {
             target="_blank"
             rel="noopener noreferrer"
           >
-            oooof this was pushed by travisCI........
+            AJAX
           </a>
         </header>
       </div>
